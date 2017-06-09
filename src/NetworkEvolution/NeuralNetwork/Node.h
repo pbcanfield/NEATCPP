a3a98346f428dd4921031c096b26abeb5dad18b2
Dave <kinbd8@gmail.com>
2017-06-09 15:49:21 -0500

    Add Node and Weight structure.

--- src/NetworkEvolution/NeuralNetwork/Node.h
+++ src/NetworkEvolution/NeuralNetwork/Node.h
@@ -1,12 +1,25 @@
 #ifndef NODE_H
 #define NODE_H
 
+#include <vector>
+#include <iostream>
+#include "Weight.h"
+
 class Node
 {
-public:
+ public:
+  void forwardProp();
+  void backwardProp();
+  
+  double sigDeriv();
+  double value();
+  
+ private:
+  std::vector<Weight*> fConnections;
+  std::vector<Weight*> bConnections;
 
-private:
-          
+  double bias;
+
 };
 
 #endif
