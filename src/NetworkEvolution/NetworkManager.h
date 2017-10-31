49aac549f1db10912743b89aa3c734eb78a12541
Pete Canfield <19PBC01@stu.cpsk12.org>
2017-10-31 16:00:22 -0500

    Add comments to the NetworkManager.h file

--- src/NetworkEvolution/NetworkManager.h
+++ src/NetworkEvolution/NetworkManager.h
@@ -12,22 +12,78 @@ public:
     ~NetworkManager();
 	
     //Required setup functions for reinforcment learning
+	
+	/**
+	 * Sets the inputs that will be used for each NeuralNetwork.
+	 * @param std::vector<std::vector<double>>  This is the inputs that are used
+	 *                                          for each NeualNetwork, each 
+	 *                                          repetition of the input data must
+	 *                                          be the size of the inputs.
+	 */
 	void setNetworkInputs(std::vector<std::vector<double>>);
+	
+	`/**
+	 * Sets the training values that will be used for each NeuralNetwork.
+	 * @param std::vector<std::vector<double>>  This is the trainging values that are used
+	 *                                          for each NeualNetwork, each 
+	 *                                          repetition of the training data must
+	 *                                          be the size of the inputs.
+	 */
 	void setNetworkTraining(std::vector<std::vector<double>>);
 	
 	//Optional setup functions
+	
+	/**
+	 * Sets the probablilities that will be used to determine the mutation rates.
+	 * @param float  This is the probablility that a mutation will occur at all.
+	 * @param float  This is the probablility that a node mutation will occur if a mutation
+	 *               occurs in the first place.
+	 * @param float  This is the probability that a connection mutation will occur if a 
+	 *               mutation occurs.
+	 * @param float  This is the probablity that a bias mutation will occur if a 
+	 *               mutation occurs.
+	 */
 	void setMutationProbability(float, float, float, float); 	
+	
+	
 	//Supervised learning functions.
-    void trainNetworksOnline(unsigned int, double=0.01);
 
+	/**
+	 * This function iterates through every network in the population, assigns the 
+	 * training data to each NeuralNetwork, then through the data an epochs number of times
+	 * at a learning rate that can be specified but defualts to 0.01.
+	 * @param unsigned int  This is the number of epochs that the NueralNetworks will be trained on.
+	 * @param double        This is the learning rate that will be used for each NeuralNetwork.
+	 */
+	void trainNetworksOnline(unsigned int, double=0.01);
+
+
+	/**
+	 * This is a function that uses the quicksort algorithm to sort every NeuralNetwork by LMSError 
+	 * from the current best performance to the worst performing network.
+	 */
     void sortSupervisedNetworks();
 
     //Genetic/Reinforcment learning functions.
 
+	/**
+	 * This is a function that the Nueral networks and allows the training process where the
+	 * NeuralNetworks are repeatedly trained, compared, and crossed with the trainNetworksOnline,
+	 * sortSupervisedNetworks, and crosstopHalf functions respectively.
+	 * @param unsigned int  This is the number of Generations that this process should repeat.
+	 * @paran unsigned int  This is the number of epochs the network will be trained.
+	 * @param double        This is the learning rate for every network (assumed to be 0.01).
+	 */
 	void reinforcementSimulate(unsigned int,unsigned int, double=0.01);
 
+	
     //Evolution functions.
-    void crossTopHalf();
+    
+	/**
+	 * This function deletes the worst half of the networks in the population and crosses the best performing
+	 * networks to replace the deleted ones.
+	 */
+	void crossTopHalf();
 
 private:
 	void quicksortVector(std::vector<NeuralNetwork*> &, int,int);
